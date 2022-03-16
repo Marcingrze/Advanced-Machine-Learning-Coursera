efa951e4aaa039a008b86efbeaceefe391462385
Marcingrze <79422088+Marcingrze@users.noreply.github.com>
2022-03-16 01:39:47 +0100

    Update keras_utils.py

--- Course_1-Introduction_to_Deep_Learning/Week-4/keras_utils.py
+++ Course_1-Introduction_to_Deep_Learning/Week-4/keras_utils.py
@@ -65,10 +65,10 @@ class ModelSaveCallback(keras.callbacks.Callback):
 
 # !!! remember to clear session/graph if you rebuild your graph to avoid out-of-memory errors !!!
 def reset_tf_session():
-    curr_session = tf.get_default_session()
+    # curr_session = tf.get_default_session()
     # close current session
-    if curr_session is not None:
-        curr_session.close()
+    #if curr_session is not None:
+    #    curr_session.close()
     # reset graph
     # K.clear_session()
     # create new session
